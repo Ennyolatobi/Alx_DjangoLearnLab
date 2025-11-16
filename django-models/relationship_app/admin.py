c199bce73673b5fd9be39e4f23c1b16a138879c7
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-11-16 23:06:16 +0100

    This is my first commit

---
+++ django-models/relationship_app/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Author, Book, Library, Librarian
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author')
+    list_filter = ('author',)
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    filter_horizontal = ('books',)  # nicer UI for M2M
+
+@admin.register(Librarian)
+class LibrarianAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'library')
