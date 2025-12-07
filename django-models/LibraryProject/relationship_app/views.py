a4d7cd079025929af50e5d4bbe71e1a12c186fcc
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-07 20:51:00 +0100

    add some features

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book, Library
 
-# Function-Based View: List all books
 def list_books_view(request):
     """List all books in the database."""
-    books = Book.objects.all()  # Query all books
-    return render(request, 'list_books.html', {'books': books})
-
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
