1c04d50bc894a2e4d002730e4aa8ee8b60e234f6
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-07 22:21:36 +0100

    add some features

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from .views import login_view, logout_view, register_view
-from .views import list_books_view, LibraryDetailView
+from .views import (
+    list_books_view,
+    LibraryDetailView,
+    login_view,
+    logout_view,
+    register_view
+)
 
 urlpatterns = [
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path("register/", register_view, name="register"),
+    path('books/', list_books_view, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-    # Existing views
-    path("books/", list_books_view, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    # auth urls
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
