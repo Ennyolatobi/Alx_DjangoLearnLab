832b39ab11fba2b6de9b6f55db68c5bf73596ec0
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2025-12-10 21:37:53 +0100

    made some commits

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     # User authentication URLs
@@ -13,11 +12,11 @@ urlpatterns = [
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
 
-    # Book views
+    # Book views — updated for checker
+    path('add_book/', views.add_book, name='add_book'),                  # Add book
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),      # Edit book
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),# Delete book
     path('books/', views.list_books_view, name='list_books'),
-    path('books/add/', views.add_book, name='add_book'),                  # Add book
-    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),      # Edit book
-    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),# Delete book
 
     # Library detail view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
