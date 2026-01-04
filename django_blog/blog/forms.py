220d6cc3742f9d5abb0c34eac5b8a4c5c5aa75eb
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2026-01-04 22:27:19 +0100

    modify post creation and update forms

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 
 class UserRegisterForm(UserCreationForm):
@@ -24,6 +25,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
 class CommentForm(forms.ModelForm):
     """
