697abc957062df3198d1a7da366f74c0592bebef
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2026-01-04 21:54:35 +0100

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
+from .models import Comment
 
 
 class UserRegisterForm(UserCreationForm):
@@ -23,3 +24,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    """
+    Form for creating and updating comments.
+    """
+    class Meta:
+        model = Comment
+        fields = ['content']
