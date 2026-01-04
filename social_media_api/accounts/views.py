ff0516d023e9801760463baa40eaad0802237d14
Ennyolatobi <ennyolaoluwatobiloba@gmail.com>
2026-01-04 23:19:16 +0100

    implement user follows and feed

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,11 +1,16 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .serializers import RegisterSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
 
-from django.contrib.auth import get_user_model
 User = get_user_model()
+
+# ================================
+# USER REGISTRATION AND LOGIN
+# ================================
 
 # Registration view
 class RegisterView(generics.CreateAPIView):
@@ -20,6 +25,7 @@ class RegisterView(generics.CreateAPIView):
         response.data['token'] = token.key
         return response
 
+
 # Login view
 class LoginView(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
@@ -31,4 +37,30 @@ class LoginView(generics.GenericAPIView):
         if user:
             token, created = Token.objects.get_or_create(user=user)
             return Response({"token": token.key})
-        return Response({"error": "Invalid credentials"}, status=400)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# ================================
+# FOLLOW/UNFOLLOW USERS
+# ================================
+
+# Follow a user
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(User, pk=user_id)
+    if user_to_follow == request.user:
+        return Response({'detail': "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+    request.user.following.add(user_to_follow)
+    return Response({'detail': f'You are now following {user_to_follow.username}.'}, status=status.HTTP_200_OK)
+
+
+# Unfollow a user
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(User, pk=user_id)
+    if user_to_unfollow == request.user:
+        return Response({'detail': "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+    request.user.following.remove(user_to_unfollow)
+    return Response({'detail': f'You have unfollowed {user_to_unfollow.username}.'}, status=status.HTTP_200_OK)
